28cea084935bb047d8b54dae557e6a67bd08a59e
Kovács Milán <95681544+Milkacola@users.noreply.github.com>
2022-03-18 12:30:26 +0100

    szimbólum kiszervezés részleges

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -7,11 +7,9 @@ a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzegh
 public class Nyugta3 {
 
     public static void main(String[] args) {
-        String csillagok = "********************";
-        System.out.println(csillagok);
-        //System.out.println("     Nyugta 3");
+        csillagok();
         System.out.printf("%14s\n", "Nyugta 3");
-        System.out.println(csillagok);
+        csillagok();
         
         final String HUF = "Ft";
         int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
@@ -22,15 +20,13 @@ public class Nyugta3 {
         System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
         System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
 
-        String duplaVonal = "====================";
-        System.out.println(duplaVonal);
+        duplaVonal();
         
         int osszesen = tetel1 + tetel2 + tetel3;
         //System.out.printf("Összesen:   %d %s\n", osszesen, huf);
         System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
         
-        String szaggatottVonal = "--------------------";
-        System.out.println(szaggatottVonal);
+        szaggatottVonal();
         
         int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
@@ -38,7 +34,7 @@ public class Nyugta3 {
         System.out.printf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
         System.out.printf("(%d%%)\n", szervizDijMertek);
         
-        System.out.println(duplaVonal);
+        duplaVonal();
         
         //int fizetendo = osszesen - szervizDij;
         int fizetendo = osszesen + szervizDij;
@@ -54,7 +50,7 @@ public class Nyugta3 {
         */
         System.out.printf("%10s%7.2f %s\n","", euro, eur);//
         
-        System.out.println(szaggatottVonal);
+        szaggatottVonal();
         
         System.out.println("");
         String rovidVonal = "_______";
@@ -66,9 +62,24 @@ public class Nyugta3 {
         System.out.print(rovidVonalValaszto);
         System.out.println("   Név");
         
-        System.out.println(csillagok);    
+        csillagok();    
         System.out.println("        CÉG");
+        csillagok();
+    }
+    
+    private static void csillagok() {
+        String csillagok = "********************";
         System.out.println(csillagok);
+    }
+    
+    private static void duplaVonal() {
+        String duplaVonal = "====================";
+        System.out.println(duplaVonal);
+    }
+    
+    private static void szaggatottVonal() {
+        String duplaVonal = "--------------------";
+        System.out.println(duplaVonal);
     }
 
 }
